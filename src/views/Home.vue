657e1392d136d8f00e0a8e89a3f34a551027a4e6
Pogo <jarpogo@gmail.com>
2020-05-21 17:02:12 -0400

    removed header and updated home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="home">
     <b-container fluid="xl">
-      <Header></Header>
+      <hr />
+      <b-jumbotron class="text-left" bg-variant="dark" text-variant="white" border-variant="dark">
+        <template v-slot:header>
+          <b-img fluid alt="Responsive image" rounded="circle" :src="require('../assets/me.png')"></b-img>
+          <span class="tab"></span>
+          Pogo
+        </template>
+
+        <hr class="my-4" />
+
+        <p>Mission Statement</p>
+      </b-jumbotron>
+
+      <hr />
     </b-container>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-
-import Header from "@/components/Header.vue";
-
-export default {
-  name: "Home",
-  components: {
-    Header
-  }
-};
-</script>
--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="projects">
-    <h1>This the personal page</h1>
+  <div class="personal">
+    <b-carousel
+      id="carousel"
+      fade
+      v-model="slide"
+      :interval="5000"
+      controls
+      indicators
+      background="#ababab"
+      max-height="100"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide :img-src="require('../assets/code.png')" img-height="100">
+        <h1>Career</h1>
+      </b-carousel-slide>
+
+      <b-carousel-slide :img-src="require('../assets/pogo.png')" img-height="100">
+        <h1>Pogo World Records</h1>
+      </b-carousel-slide>
+
+      <b-carousel-slide :img-src="require('../assets/pichu.png')" img-height="100">
+        <h1>Smash Competitor</h1>
+      </b-carousel-slide>
+    </b-carousel>
   </div>
 </template>
+
+
+// caption="First slide"
+//         text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+
+
+<script>
+export default {
+  data() {
+    return {
+      slide: 0,
+      sliding: null
+    };
+  },
+  methods: {
+    onSlideStart() {
+      this.sliding = true;
+    },
+    onSlideEnd() {
+      this.sliding = false;
+    }
+  }
+};
+</script>
