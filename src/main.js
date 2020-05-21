43f74bfebfbf89f18d3e794b1e18e243fb2e69f8
Pogo <jarpogo@gmail.com>
2020-05-21 17:24:09 -0400

    updated the favicon

--- src/main.js
+++ src/main.js
@@ -2,14 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { BootstrapVue, IconsPlugin, LinkPlugin } from 'bootstrap-vue'
+import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 
 Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-Vue.use(LinkPlugin)
 Vue.config.productionTip = false
 
 new Vue({
